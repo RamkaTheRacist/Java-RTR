1d668747031c9b62b3fceaadd239a32c7a1c6907
Ramka <whereisourgod@gmail.com>
2022-09-08 03:21:50 +0300

    task1

--- J_se_les2/HW/task1.java
+++ J_se_les2/HW/task1.java
@@ -1,5 +1,21 @@
+//Написать программу вычисления n-ого треугольного числа
 package Java.J_se_les2.HW;
 
+import java.util.Scanner;
+
 public class task1 {
-    
+    static int countNTNumber(int n) {
+        int tmp = 0;
+        for (int i = 1; i <= n; i++)
+            tmp += i;
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("n: ");
+        int number = iScanner.nextInt();
+        iScanner.close();
+        System.out.printf("T(%d) = %d ", number, countNTNumber(number));
+    }
 }
