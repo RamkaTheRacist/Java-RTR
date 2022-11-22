832b50c8e49ff736e627e599674957f1062817a0
Roman Kurganskii <103903221+RamkaTheRacist@users.noreply.github.com>
2022-11-22 18:09:19 +1000

    Update Program.cs

--- J_se_les13/HW/Program.cs
+++ J_se_les13/HW/Program.cs
@@ -7,48 +7,7 @@ namespace HW.Client
         static void Main(string[] args)
         {
             new AppV1().StartApp();
-            // ContactV1 any1 = new ContactV1("f1", "f1", "f1", 123, "f1");
-            // ContactV1 any2 = new ContactV1("d1", "d1", "d1", 223, "d1");
-            // ContactV1 any3 = new ContactV1("f1", "f1", "f1", 123, "f1");
-            // PhonebookV1 tmp = new PhonebookV1();
-
             
-            // string path = "C:\\Users\\where\\Desktop\\Git\\Java\\J_se_les13\\HW\\Files\\tmp.txt";
-            // ImportV1 imp = new ImportV1(tmp,path);
-            // imp.ImportFiles();
-            // System.Console.WriteLine(tmp.SizeOfBook());
-            // AddV1 add = new AddV1(tmp, new ConsoleInputV1(), new ConsoleOutSucV1(), new ConsoleOutErrorV1());
-            // //add.Add();
-            // System.Console.WriteLine(tmp.SizeOfBook());
-            // SaveV1 save = new SaveV1(tmp);
-            // save.Save(new GetTypeV1().GetType(path), path);
-            // string aglskje = "fff.csv";
-            // save.Save(new GetTypeV1().GetType(aglskje), new GetPathV1().GetPath(path) + aglskje);
-            // //add.Add();
-            // System.Console.WriteLine(tmp.SizeOfBook());
-
-
-
-
-            // tmp.Add(any1);
-            // tmp.Add(any2);
-            // tmp.Add(any3);
-            // System.Console.WriteLine($"{tmp.SizeOfBook()}");
-            // System.Console.WriteLine($"{tmp.GetContact(0)}");
-            // System.Console.WriteLine($"{tmp.GetContact(1)}");
-            // System.Console.WriteLine($"{tmp.GetContact(2)}");
-            // System.Console.WriteLine($"{tmp.GetContactIndex(any3)}");
-            // System.Console.WriteLine($"{tmp.IfExist(any2)}");
-            // tmp.Remove(1);
-            // System.Console.WriteLine($"{tmp.SizeOfBook()}");
-            // System.Console.WriteLine($"{tmp.GetContact(0)}");
-            // System.Console.WriteLine($"{tmp.GetContact(1)}");
-            // System.Console.WriteLine($"{tmp.IfExist(any2)}");
-
-
-
-
-
         }
     }
 }
