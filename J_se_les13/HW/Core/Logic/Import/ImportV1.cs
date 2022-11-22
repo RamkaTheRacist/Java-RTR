56bd50c1214b05812f0b19d4a2e3e714f64b6879
Roman Kurganskii <103903221+RamkaTheRacist@users.noreply.github.com>
2022-11-22 18:12:55 +1000

    Update ImportV1.cs

--- J_se_les13/HW/Core/Logic/Import/ImportV1.cs
+++ J_se_les13/HW/Core/Logic/Import/ImportV1.cs
@@ -44,50 +44,5 @@ namespace HW.Core.Logic.Import
 
             }
         }
-        //File file = new File(FilePath);
     }
 }
-
-/*
-import Java.J_se_les12.HW.Core.Contact.Contact;
-import Java.J_se_les12.HW.Core.Logic.GetType.GetType;
-import Java.J_se_les12.HW.Core.Phonebook.Phonebook;
-
-public class ImportV1 extends ImportBase {
-    public ImportV1(String path, Phonebook phonebook) {
-        super(path, phonebook);
-    }
-
-    @Override
-    public void importFiles() {
-        try {
-            File file = new File(getPath());
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            String data = reader.readLine();
-            String split = "";
-            String type = new GetType().getType(getPath());
-            if (type.equals("csv")) {
-                split = ";";
-            } else if (type.equals("txt")) {
-                split = " ";
-            }
-            while (data != null) {
-                String[] tmp = data.split(split);
-                String fn = tmp[0];
-                String ln = tmp[1];
-                String or = tmp[2];
-                Integer num = Integer.parseInt(tmp[3]);
-                String no = tmp[4];
-                this.phonebook.add(new Contact(fn, ln, or, num, no));
-                data = reader.readLine();
-            }
-            reader.close();
-            fr.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-}*/
